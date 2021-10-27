efd205a8abc711887155ec88d38810c929d49990
Arnold Chand <creativenull@outlook.com>
2021-10-27 12:19:36 -0400

    fix(linters/gcc): add extra flags

--- lua/efmls-configs/linters/gcc.lua
+++ lua/efmls-configs/linters/gcc.lua
@@ -1,7 +1,7 @@
 return {
   prefix = 'gcc',
-  lintCommand = 'gcc -Wall -fno-diagnostics-color ${INPUT}',
+  lintCommand = 'gcc -Wpedantic -Wall -Wextra -fno-diagnostics-color -fsyntax-only ${INPUT}',
   lintStdin = false,
   lintFormats = { '%.%#:%l:%c: %trror: %m', '%.%#:%l:%c: %tarning: %m', '%.%#:%l:%c: %tnfo: %m' },
-  rootMarkers = {},
+  rootMarkers = { 'Makefile' },
 }
