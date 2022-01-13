9ecfefe1230ba3a2467334d2fb26d4c205aa575e
Arnold Chand <creativenull@outlook.com>
2022-01-13 00:09:59 -0400

    feat(formatters/fecs): new css/html/js/ts formatter

---
+++ lua/efmls-configs/formatters/fecs.lua
@@ -0,0 +1,14 @@
+local fs = require('efmls-configs.fs')
+
+-- TODO:
+-- This formatter leave it's tooling information
+-- at the line. Check for workarounds, if possible.
+
+local formatter = 'fecs'
+local args = 'format --silent --ignore --stream ${INPUT}'
+local command = string.format('%s %s', fs.executable(formatter, fs.Scope.NODE), args)
+
+return {
+  formatCommand = command,
+  formatStdin = true,
+}
