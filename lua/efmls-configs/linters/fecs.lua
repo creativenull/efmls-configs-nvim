4d208cc43060cddc02ea3f33eca96f07662570dc
Arnold Chand <creativenull@outlook.com>
2022-01-13 00:09:42 -0400

    feat(linters/fecs): new css/html/js/ts linter

---
+++ lua/efmls-configs/linters/fecs.lua
@@ -0,0 +1,18 @@
+local fs = require('efmls-configs.fs')
+
+local linter = 'fecs'
+local bin = fs.executable(linter, fs.Scope.NODE)
+local args = 'check --color=false --stream ${INPUT}'
+local command = string.format('%s %s', bin, args)
+
+return {
+  prefix = linter,
+  lintCommand = command,
+  lintStdin = true,
+  lintFormats = {
+    'fecs [%tRROR] → line %l, col %c: %m',
+    'fecs [%tARNING] → line %l, col %c: %m',
+    'fecs [%tNFO] → line %l, col %c: %m',
+  },
+  rootMarkers = {},
+}
