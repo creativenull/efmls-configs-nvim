818c4c656e341d469677ae81fd09d11160e73eac
Arnold Chand <creativenull@outlook.com>
2022-01-12 22:22:31 -0400

    feat(linters/clj-kondo): add clj-kondo

---
+++ lua/efmls-configs/linters/clj_kondo.lua
@@ -0,0 +1,12 @@
+local fs = require('efmls-configs.fs')
+
+local linter = 'clj-kondo'
+local command = string.format('%s --lint -', fs.executable(linter))
+
+return {
+  prefix = linter,
+  lintCommand = command,
+  lintStdin = true,
+  lintFormats = { '%.%#:%l:%c: %trror: %m', '%.%#:%l:%c: %tarning: %m', '%.%#:%l:%c: %tote: %m' },
+  rootMarkers = { '.clj-kondo/config.edn' },
+}
