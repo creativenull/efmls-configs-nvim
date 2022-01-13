1d59d19429287652d265c82053457515add8a100
Arnold Chand <creativenull@outlook.com>
2022-01-12 22:41:49 -0400

    feat(linters/joker): clojure linter

---
+++ lua/efmls-configs/linters/joker.lua
@@ -0,0 +1,12 @@
+local fs = require('efmls-configs.fs')
+
+local linter = 'joker'
+local command = string.format('%s --lint -', fs.executable(linter))
+
+return {
+  prefix = linter,
+  lintCommand = command,
+  lintStdin = true,
+  lintFormats = { '%.%#:%l:%c: Read %trror: %m', '%.%#:%l:%c: Parse %trror: %m', '%.%#:%l:%c: Parse %tarning: %m' },
+  rootMarkers = { '.joker' },
+}
