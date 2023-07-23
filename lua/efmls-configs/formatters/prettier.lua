f0e1430037df3f3e876c301a163bcc2dc89e603b
L Lllvvuu <git@llllvvuu.dev>
2023-07-22 17:37:51 -0700

    feat(prettier, prettier_d): textDocument/rangeFormatting

--- lua/efmls-configs/formatters/prettier.lua
+++ lua/efmls-configs/formatters/prettier.lua
@@ -1,9 +1,25 @@
 local fs = require('efmls-configs.fs')
 
 local formatter = 'prettier'
-local command = string.format('%s --stdin --stdin-filepath ${INPUT}', fs.executable(formatter, fs.Scope.NODE))
+local command = string.format(
+  '%s --stdin --stdin-filepath ${INPUT} ${--range-start:charStart} '
+    .. '${--range-end:charEnd} ${--tab-width:tabSize} ${--use-tabs:!insertSpaces}',
+  fs.executable(formatter, fs.Scope.NODE)
+)
 
 return {
+  formatCanRange = true,
   formatCommand = command,
   formatStdin = true,
+  rootMarkers = {
+    ".prettierrc",
+    ".prettierrc.json",
+    ".prettierrc.js",
+    ".prettierrc.yml",
+    ".prettierrc.yaml",
+    ".prettierrc.json5",
+    ".prettierrc.mjs",
+    ".prettierrc.cjs",
+    ".prettierrc.toml",
+  },
 }
--- lua/efmls-configs/formatters/prettier_d.lua
+++ lua/efmls-configs/formatters/prettier_d.lua
@@ -1,9 +1,15 @@
 local fs = require('efmls-configs.fs')
 
 local formatter = 'prettierd'
-local command = string.format('%s ${INPUT}', fs.executable(formatter, fs.Scope.NODE))
+local command = string.format(
+  '%s ${INPUT} ${--range-start=charStart} ${--range-end=charEnd} '
+    .. '${--tab-width=tabSize} ${--use-tabs=!insertSpaces}',
+  fs.executable(formatter, fs.Scope.NODE)
+)
 
 return {
+  formatCanRange = true,
   formatCommand = command,
   formatStdin = true,
+  rootMarkers = require("efmls-configs.formatters.prettier").rootMarkers,
 }
