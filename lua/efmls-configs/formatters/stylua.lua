12d9edda80400a056bd90b99259fb91c0f9300b3
LW <git@llllvvuu.dev>
2023-07-23 10:50:19 -0700

    feat(stylua): textDocument/rangeFormatting (#15)

--- lua/efmls-configs/formatters/stylua.lua
+++ lua/efmls-configs/formatters/stylua.lua
@@ -1,9 +1,14 @@
 local fs = require('efmls-configs.fs')
 
 local formatter = 'stylua'
-local command = string.format('%s --color Never -', fs.executable(formatter))
+local command = string.format(
+  '%s ${--indent-width:tabSize} ${--range-start:charStart} '
+    .. '${--range-end:charEnd} --color Never -',
+  fs.executable(formatter)
+)
 
 return {
+  formatCanRange = true,
   formatCommand = command,
   formatStdin = true,
   rootMarkers = { 'stylua.toml', '.stylua.toml' },
