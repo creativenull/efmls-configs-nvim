b6da02294db7503e5a659797617a0872243eac84
LW <git@llllvvuu.dev>
2023-07-27 17:54:22 -0700

    fix(shfmt): pass in input filename (#17)
    If `shfmt` doesn't have the input filename, it won't find `.editorconfig`

--- lua/efmls-configs/formatters/shfmt.lua
+++ lua/efmls-configs/formatters/shfmt.lua
@@ -1,7 +1,7 @@
 local fs = require('efmls-configs.fs')
 
 local formatter = 'shfmt'
-local command = string.format('%s -', fs.executable(formatter))
+local command = string.format('%s -filename ${INPUT} -', fs.executable(formatter))
 
 return {
   formatCommand = command,
