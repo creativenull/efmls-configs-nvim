a4a3bf748cc219b3c5da1a523813c840ba4e3817
Penaz <penazarea@altervista.org>
2023-08-01 15:56:25 +0200

    Added djlint Support

---
+++ lua/efmls-configs/linters/djlint.lua
@@ -0,0 +1,15 @@
+local fs = require('efmls-configs.fs')
+
+local linter = 'djlint'
+local bin = fs.executable(linter)
+local args = [[--linter-output-format 'I:{line}: {message}' --profile=django -]]
+local command = string.format('%s %s', bin, args)
+
+return {
+    prefix = linter,
+    lintCommand = command,
+    lintFormats = {'%t:%l:%c: %m'},
+    lintStdin = true,
+    lintIgnoreExitCode = true,
+    lintDebounce = "1s",
+}
