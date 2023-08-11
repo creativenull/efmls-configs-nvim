f7b6647d745e5619624c2402f7ffd1f11c30fcc9
Arnold Chand <creativenull@outlook.com>
2023-08-11 13:04:24 -0400

    feat(init): no impl, add msg for new setup

--- lua/efmls-configs/init.lua
+++ lua/efmls-configs/init.lua
@@ -1,6 +1,10 @@
+require('efmls-configs.logger').info(
+  '[efmls-configs] init() and setup() is no longer required, check `:help efmls-configs-setup` for the new way.'
+)
+
 local lspconfig_ok, lspconfig = pcall(require, 'lspconfig')
 if not lspconfig_ok then
-  local errmsg = '[efmls-configs] `nvim-lspconfig` plugin not installed! Please install via your plugin manager.'
+  local errmsg = '[efmls-configs] `nvim-lspconfig` plugin is required! Please install via your plugin manager.'
   vim.api.nvim_err_writeln(errmsg)
   return
 end
@@ -8,44 +12,45 @@ end
 local M = {}
 
 ---@class EfmLSConfig
-local efmls_setup = {
-  root_dir = lspconfig.util.root_pattern('.git'),
-  init_options = {
-    documentFormatting = false,
-  },
-  settings = {
-    rootMarkers = { '.git' },
-    languages = {},
-  },
-
-  -- Plugin options
-  default_config = false,
-}
+-- local efmls_setup = {
+--   root_dir = lspconfig.util.root_pattern('.git'),
+--   init_options = {
+--     documentFormatting = false,
+--   },
+--   settings = {
+--     rootMarkers = { '.git' },
+--     languages = {},
+--   },
+--
+--   -- Plugin options
+--   default_config = false,
+-- }
 
 ---Insert the relevant linter/formatter into the source provided
 ---@param source table
 ---@param tools table
-local insert_settings = function(source, tools)
-  if vim.tbl_islist(tools) then
-    for _, tool in pairs(tools) do
-      table.insert(source, tool)
-    end
-  else
-    table.insert(source, tools)
-  end
-end
+-- local insert_settings = function(source, tools)
+--   if vim.tbl_islist(tools) then
+--     for _, tool in pairs(tools) do
+--       table.insert(source, tool)
+--     end
+--   else
+--     table.insert(source, tools)
+--   end
+-- end
 
 ---Initalize efm-langserver LSP options for user
 ---@param user_efmls_opts EfmLSConfig
 M.init = function(user_efmls_opts)
-  if user_efmls_opts ~= nil or user_efmls_opts ~= {} then
-    efmls_setup = vim.tbl_extend('force', efmls_setup, user_efmls_opts)
-  end
+  -- if user_efmls_opts ~= nil or user_efmls_opts ~= {} then
+  --   efmls_setup = vim.tbl_extend('force', efmls_setup, user_efmls_opts)
+  -- end
 end
 
 ---Setup efm-langserver settings for each filetype provided
 ---@param filetypes table
 M.setup = function(filetypes)
+  --[[
   -- Merge default config if required
   -- but priority on filetypes specified first
   if efmls_setup.default_config then
@@ -78,6 +83,7 @@ M.setup = function(filetypes)
   end
 
   lspconfig.efm.setup(efmls_setup)
+  --]]
 end
 
 return M
