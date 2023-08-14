28153116c27e42a7ccb33e27c3d4eeef09179c82
jared <jared.j.barnak@gmail.com>
2023-08-14 03:09:52 -0400

    feat(formatter): Add smlfmt
    `smlfmt` needlessly appends and prepends lines, hence the need for
    `sed` deleting those lines.

---
+++ lua/efmls-configs/formatters/smlfmt.lua
@@ -0,0 +1,13 @@
+-- Metadata
+-- languages: sml
+-- url: https://github.com/shwestrick/smlfmt
+
+local fs = require("efmls-configs.fs")
+
+local formatter = "smlfmt"
+local command = string.format("%s --preview-only ${FILENAME} | sed '1d;$d'", fs.executable(formatter))
+
+return {
+  formatCommand = command,
+  formatStdin = true,
+}
