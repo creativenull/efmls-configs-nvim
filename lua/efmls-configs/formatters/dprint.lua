cc0a737ccd69fcb85cb2dfd7f3133a8b4c7adc9c
Maria José Solano <majosolano99@gmail.com>
2023-08-17 15:11:39 -0700

    feat(formatter): add dprint configuration (#34)

---
+++ lua/efmls-configs/formatters/dprint.lua
@@ -0,0 +1,19 @@
+-- Metadata
+-- languages: javascript,typescript,json,markdown,toml,rust,roslyn
+-- url: https://dprint.dev/
+
+local fs = require('efmls-configs.fs')
+
+local formatter = 'dprint'
+local command = string.format('%s fmt --stdin ${INPUT}', fs.executable(formatter, fs.Scope.NODE))
+
+return {
+  formatCommand = command,
+  formatStdin = true,
+  rootMarkers = {
+    'dprint.json',
+    'dprint.jsonc',
+    '.dprint.json',
+    '.dprint.jsonc',
+  },
+}
