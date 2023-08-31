f3936acd08f1b94f895569f74e713ff423b94ba6
dimitris <dimitris@192.168.1.32>
2023-09-01 00:16:46 +0300

    add latexindent formatter

---
+++ lua/efmls-configs/formatters/latexindent.lua
@@ -0,0 +1,14 @@
+-- Metadata
+-- languages: tex
+-- url: https://github.com/cmhughes/latexindent.pl
+
+local fs = require('efmls-configs.fs')
+
+local formatter = 'latexindent'
+local command = string.format('%s -', fs.executable(formatter))
+
+return {
+  prefix = formatter,
+  formatCommand = command,
+  formatStdin = true,
+}
