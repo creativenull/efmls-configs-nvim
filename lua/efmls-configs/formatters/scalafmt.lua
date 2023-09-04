bbf336a39490f076ebbf293c1353ae39b6f28bef
Pedro Mendes <pedrohrmendes@proton.me>
2023-09-04 18:26:10 -0300

    Adding scalafmt support.

---
+++ lua/efmls-configs/formatters/scalafmt.lua
@@ -0,0 +1,15 @@
+-- Metadata
+-- languages: scala
+-- url: https://scalameta.org/scalafmt/
+
+local fs = require('efmls-configs.fs')
+
+local formatter = 'scalafmt'
+local args = '--stdin'
+local command = string.format(fs.executable(formatter), args)
+
+return {
+  formatCanRange = true,
+  formatCommand = command,
+  formatStdin = true,
+}
