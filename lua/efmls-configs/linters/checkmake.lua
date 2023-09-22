2bfac79c8a61de574d2d7d31f39a919a3600cc4a
Václav Kubernát <sir.venceslas@gmail.com>
2023-09-22 22:09:51 +0200

    feat(linter): add checkmake (#83)

---
+++ lua/efmls-configs/linters/checkmake.lua
@@ -0,0 +1,18 @@
+-- Metadata
+-- languages: make
+-- url: https://github.com/mrtazz/checkmake
+
+local sourceText = require('efmls-configs.utils').sourceText
+local fs = require('efmls-configs.fs')
+
+local linter = 'checkmake'
+local command = string.format('%s --format=\'{{.LineNumber}}:{{.Violation}} ({{.Rule}})\n\' "${INPUT}"', fs.executable(linter))
+
+return {
+  prefix = linter,
+  lintSource = sourceText(linter),
+  lintCommand = command,
+  lintStdin = false,
+  lintFormats = { '%l:%m' },
+  rootMarkers = {},
+}
