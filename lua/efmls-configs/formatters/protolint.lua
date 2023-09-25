d6bef40a30c5c42570043ffacd87dbe934fe43c4
Mitch <mitchjoa@gmail.com>
2023-09-25 12:05:59 -0400

    feat(formatters): add protolint (#85)

---
+++ lua/efmls-configs/formatters/protolint.lua
@@ -0,0 +1,17 @@
+-- Metadata
+-- languages: proto
+-- url: https://github.com/yoheimuta/protolint
+
+local fs = require('efmls-configs.fs')
+
+local formatter = 'protolint'
+local command = string.format("%s lint -fix '${INPUT}'", fs.executable(formatter))
+
+return {
+  formatCommand = command,
+  formatStdin = false,
+  rootMarkers = {
+    '.protolint.yaml',
+    '.protolint.yml',
+  },
+}
