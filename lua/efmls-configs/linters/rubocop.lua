a5f8269b1d2ad717be57facb399d408efe196466
Kassio Borges <kassioborgesm@gmail.com>
2023-10-02 23:24:25 +0200

    Make rubocop configurable

--- lua/efmls-configs/linters/rubocop.lua
+++ lua/efmls-configs/linters/rubocop.lua
@@ -6,13 +6,28 @@ local sourceText = require('efmls-configs.utils').sourceText
 local fs = require('efmls-configs.fs')
 
 local linter = 'rubocop'
-local command = string.format('%s --lint --format emacs --stdin "${INPUT}"', fs.executable(linter, fs.Scope.BUNDLE))
 
-return {
+local function commandBuilder(args)
+  args = args or '--lint --format emacs --stdin "${INPUT}"'
+
+  return string.format('%s %s', fs.executable(linter, fs.Scope.BUNDLE), args)
+end
+
+local data = {
   prefix = linter,
   lintSource = sourceText(linter),
-  lintCommand = command,
+  lintCommand = commandBuilder(),
   lintStdin = true,
   lintFormats = { '%f:%l:%c: %t: %m' },
   rootMarkers = {},
 }
+
+return setmetatable(data, {
+  __call = function(t, opts)
+    if opts.args ~= nil then
+      t.lintCommand = commandBuilder(opts.args)
+    end
+
+    return vim.tbl_extend('force', t, opts.override or {})
+  end,
+})
