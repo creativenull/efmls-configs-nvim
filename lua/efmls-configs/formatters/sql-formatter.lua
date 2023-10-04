082c4040c3d56c9ef56e1e7142000438eb542caf
Anthony Fiddes <11233666+Anthony-Fiddes@users.noreply.github.com>
2023-10-03 20:34:17 -0700

    feat(formatter): Add sql-formatter config (#90)

---
+++ lua/efmls-configs/formatters/sql-formatter.lua
@@ -0,0 +1,13 @@
+-- Metadata
+-- languages: sql
+-- url: https://github.com/sql-formatter-org/sql-formatter
+
+local fs = require('efmls-configs.fs')
+
+local formatter = 'sql-formatter'
+local command = string.format('%s', fs.executable(formatter, fs.Scope.NODE))
+
+return {
+  formatCommand = command,
+  formatStdin = true,
+}
