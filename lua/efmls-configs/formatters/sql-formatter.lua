96ea0060995c20756e223e32634094e13fac1400
Anthony Fiddes <11233666+Anthony-Fiddes@users.noreply.github.com>
2023-10-03 18:53:59 -0700

    Remove opinionated configuration

--- lua/efmls-configs/formatters/sql-formatter.lua
+++ lua/efmls-configs/formatters/sql-formatter.lua
@@ -3,19 +3,10 @@
 -- url: https://github.com/sql-formatter-org/sql-formatter
 
 local fs = require('efmls-configs.fs')
-local ok, Path = pcall(require, 'plenary.path')
-
-local args = ''
-if ok then
-  local cfg = Path:new(vim.fn.stdpath('config'), 'sql-formatter.json')
-  if cfg:exists() and cfg:is_file() then
-    args = string.format('--config %s', cfg)
-  end
-end
 
 local formatter = 'sql-formatter'
 
-local command = string.format('%s %s', fs.executable(formatter, fs.Scope.NODE), args)
+local command = string.format('%s', fs.executable(formatter, fs.Scope.NODE))
 
 return {
   formatCommand = command,
