7db13d3f609640e9f12d7d6f1251e6a8f964f579
Arnold Chand <creativenull@outlook.com>
2023-10-23 19:29:58 -0400

    fix(blade-formatter): use stdin (#97)

--- lua/efmls-configs/formatters/blade_formatter.lua
+++ lua/efmls-configs/formatters/blade_formatter.lua
@@ -5,9 +5,9 @@
 local fs = require('efmls-configs.fs')
 
 local formatter = 'blade-formatter'
-local command = string.format("%s '${INPUT}'", fs.executable(formatter))
+local command = string.format("%s --stdin", fs.executable(formatter))
 
 return {
   formatCommand = command,
-  formatStdin = false,
+  formatStdin = true,
 }
