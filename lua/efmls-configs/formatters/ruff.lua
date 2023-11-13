745e8241f738dd006666a643560e262291102c42
Gaétan Lepage <33058747+GaetanLepage@users.noreply.github.com>
2023-11-13 14:07:56 +0100

    fix(formatter/ruff): fix args (#102)

--- lua/efmls-configs/formatters/ruff.lua
+++ lua/efmls-configs/formatters/ruff.lua
@@ -5,11 +5,16 @@
 local fs = require('efmls-configs.fs')
 
 local formatter = 'ruff'
-local args = "check --fix-only --no-cache --stdin-filename '${INPUT}'"
+local args = "format --no-cache --stdin-filename '${INPUT}'"
 local command = string.format('%s %s', fs.executable(formatter), args)
 
 return {
   formatCommand = command,
   formatStdin = true,
-  rootMarkers = { 'pyproject.toml', 'setup.py', 'requirements.txt' },
+  rootMarkers = {
+    'pyproject.toml',
+    'setup.py',
+    'requirements.txt',
+    'ruff.toml'
+  },
 }
