7b79dfaaa40b32b3092c7b3cc872d37fc3412c66
Arnold Chand <creativenull@outlook.com>
2023-11-28 09:47:03 -0400

    fix(formatter/black): use old cmd on windows

--- lua/efmls-configs/formatters/black.lua
+++ lua/efmls-configs/formatters/black.lua
@@ -5,14 +5,19 @@
 local fs = require('efmls-configs.fs')
 
 local formatter = 'black'
-
 local command = string.format(
   "%s --no-color -q $(echo ${--useless:rowStart} ${--useless:rowEnd} | xargs -n4 -r sh -c 'echo --line-ranges=$(($1+1))-$(($3+1))') -",
   fs.executable(formatter)
 )
 
+-- TODO: add range formatting for windows without relying on `xargs`
+local is_windows = vim.fn.has('win32') == 1
+if is_windows then
+  command = string.format('%s --no-color -q -', fs.executable(formatter))
+end
+
 return {
-  formatCanRange = true,
+  formatCanRange = not is_windows,
   formatCommand = command,
   formatStdin = true,
 }
