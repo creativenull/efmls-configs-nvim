7f635de1e62636410867ac968c5ed4e56650a2dd
kuator <kuatabylkasymov@gmail.com>
2023-11-28 19:40:09 +0600

    feat(formatter/black): support range formatting (#109)

--- lua/efmls-configs/formatters/black.lua
+++ lua/efmls-configs/formatters/black.lua
@@ -5,9 +5,14 @@
 local fs = require('efmls-configs.fs')
 
 local formatter = 'black'
-local command = string.format('%s --no-color -q -', fs.executable(formatter))
+
+local command = string.format(
+  "%s --no-color -q $(echo ${--useless:rowStart} ${--useless:rowEnd} | xargs -n4 -r sh -c 'echo --line-ranges=$(($1+1))-$(($3+1))') -",
+  fs.executable(formatter)
+)
 
 return {
+  formatCanRange = true,
   formatCommand = command,
   formatStdin = true,
 }
