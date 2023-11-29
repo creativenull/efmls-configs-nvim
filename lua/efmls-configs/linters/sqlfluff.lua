f628a60ad482fdbc7d3bcc787c3cfe66813c8718
Arnold Chand <creativenull@outlook.com>
2023-11-28 22:46:06 -0400

    feat(linter): add sqlfluff (#111)

---
+++ lua/efmls-configs/linters/sqlfluff.lua
@@ -0,0 +1,23 @@
+-- Metadata
+-- languages: sql
+-- url: https://github.com/sqlfluff/sqlfluff
+
+local fs = require('efmls-configs.fs')
+
+local linter = 'sqlfluff'
+local command = string.format(
+  '%s lint --dialect ansi --format github-annotation-native --annotation-level warning --nocolor --disable-progress-bar ${INPUT}',
+  fs.executable(linter)
+)
+
+return {
+  prefix = linter,
+  lintCommand = command,
+  lintIgnoreExitCode = true,
+  lintStdin = false,
+  lintFormats = {
+    '::%totice title=SQLFluff,file=%f,line=%l,col=%c::%m',
+    '::%tarning title=SQLFluff,file=%f,line=%l,col=%c::%m',
+    '::%trror title=SQLFluff,file=%f,line=%l,col=%c::%m',
+  },
+}
