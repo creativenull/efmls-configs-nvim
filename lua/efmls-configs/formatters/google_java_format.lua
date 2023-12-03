cfaf1ae0f75eb79a8fb6ab53a5e4da3eb0fc8a9f
kuator <kuatabylkasymov@gmail.com>
2023-12-03 17:12:29 +0600

    Update google_java_format.lua

--- lua/efmls-configs/formatters/google_java_format.lua
+++ lua/efmls-configs/formatters/google_java_format.lua
@@ -3,6 +3,7 @@
 -- url: https://github.com/google/google-java-format
 
 local fs = require('efmls-configs.fs')
+-- TODO range formatting https://github.com/mskelton/null-ls.nvim/commit/ea1a4539
 
 local formatter = 'google-java-format'
 local command = string.format('%s -', fs.executable(formatter))
