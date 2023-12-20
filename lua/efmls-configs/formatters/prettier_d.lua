1678c05966eb0464f99c062071aa278e1e390549
HeRo3Sm <hungid6a1@gmail.com>
2023-12-21 05:56:39 +0700

    fix(formatter/prettier): fix indent arg for prettier and prettier_d

--- lua/efmls-configs/formatters/prettier_d.lua
+++ lua/efmls-configs/formatters/prettier_d.lua
@@ -7,7 +7,7 @@ local fs = require('efmls-configs.fs')
 local formatter = 'prettierd'
 local command = string.format(
   "%s '${INPUT}' ${--range-start=charStart} ${--range-end=charEnd} "
-    .. '${--tab-width=tabSize} ${--use-tabs=!insertSpaces}',
+    .. '${--tab-width=tabWidth} ${--use-tabs=!insertSpaces}',
   fs.executable(formatter, fs.Scope.NODE)
 )
 
