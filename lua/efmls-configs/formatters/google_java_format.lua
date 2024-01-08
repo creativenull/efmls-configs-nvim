b2be01ff8ac65ba8aba632bd3b18ee0920bdcf61
kuator <kuatabylkasymov@gmail.com>
2024-01-08 12:01:35 +0600

    Update google_java_format.lua
    Stole code from kimabrandt's comment: https://github.com/creativenull/efmls-configs-nvim/pull/113#issuecomment-1877744734

--- lua/efmls-configs/formatters/google_java_format.lua
+++ lua/efmls-configs/formatters/google_java_format.lua
@@ -3,12 +3,33 @@
 -- url: https://github.com/google/google-java-format
 
 local fs = require('efmls-configs.fs')
--- TODO range formatting https://github.com/mskelton/null-ls.nvim/commit/ea1a4539
 
 local formatter = 'google-java-format'
-local command = string.format('%s -', fs.executable(formatter))
+
+
+local command = fs.executable("google-java-format")
+    .. " --aosp" -- Use AOSP style instead of Google Style (4-space indentation).
+    .. " $(echo -n ${--useless:rowStart} ${--useless:rowEnd}"
+    .. " | xargs -n4 -r sh -c 'echo"
+    .. " --skip-sorting-imports" -- Do not fix the import order.
+    .. " --skip-removing-unused-imports" -- Do not remove unused imports.
+    .. " --skip-reflowing-long-strings" -- Do not reflow string literals that exceed the column limit.
+    .. " --skip-javadoc-formatting" -- Do not reformat javadoc.
+    .. " --lines $(($1+1)):$(($3+1))'" -- Line range(s) to format, like 5:10 (1-based; default is all).
+    .. ") -"
+
+local is_windows = vim.fn.has('win32') == 1
+if is_windows then
+  command = string.format('%s -', fs.executable(formatter))
+end
 
 return {
+  formatCanRange = not is_windows,
   formatCommand = command,
   formatStdin = true,
+  rootMarkers = {
+      ".project",
+      "classpath",
+      "pom.xml",
+  },
 }
