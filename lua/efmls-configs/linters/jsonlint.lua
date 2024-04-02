479505abe49a554c0d31104a2fee730188240944
Arnold Chand <creativenull@outlook.com>
2024-04-02 10:46:43 -0400

    fix(linters): lintFormats for jsonlint and markuplint (#130)

--- lua/efmls-configs/linters/jsonlint.lua
+++ lua/efmls-configs/linters/jsonlint.lua
@@ -6,14 +6,14 @@ local sourceText = require('efmls-configs.utils').sourceText
 local fs = require('efmls-configs.fs')
 
 local linter = 'jsonlint'
-local args = '--sort-keys  --compact --validate ${INPUT}'
+local args = '--sort-keys --compact'
 local command = string.format('%s %s', fs.executable(linter, fs.Scope.NODE), args)
 
 return {
-	prefix = linter,
-	lintSource = sourceText(linter),
-	lintCommand = command,
-	lintStdin = true,
-	lintFormats = { '%.%#: line %l, col %c, %trror - %m', '%.%#: line %l, col %c, %tarning - %m' },
-	rootMarkers = { '.jsonlintrc' },
+  prefix = linter,
+  lintSource = sourceText(linter),
+  lintCommand = command,
+  lintStdin = true,
+  lintFormats = { 'line %l, col %c, %m' },
+  rootMarkers = { '.jsonlintrc' },
 }
