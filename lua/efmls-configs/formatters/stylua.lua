6123245ae82a51b2facd45e712f0028ba6294a56
Arnold Chand <creativenull@outlook.com>
2024-12-04 14:17:00 -0400

    fix(stylua): use --stdin-filepath option

--- lua/efmls-configs/formatters/stylua.lua
+++ lua/efmls-configs/formatters/stylua.lua
@@ -5,8 +5,10 @@
 local fs = require('efmls-configs.fs')
 
 local formatter = 'stylua'
-local command =
-  string.format('%s --color Never ${--range-start:charStart} ${--range-end:charEnd} -', fs.executable(formatter))
+local command = string.format(
+  "%s --color Never ${--range-start:charStart} ${--range-end:charEnd} --stdin-filepath '${INPUT}' -",
+  fs.executable(formatter)
+)
 
 return {
   formatCanRange = true,
