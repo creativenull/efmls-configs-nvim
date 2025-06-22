2a0873b3f8ade365f1440447bacc9afec61b48b9
Shubham Pawar <31388020+shubham-cpp@users.noreply.github.com>
2025-06-22 15:26:44 +0000

    fix(linter/golangci_lint): not working with version 2 (#151)

--- lua/efmls-configs/linters/golangci_lint.lua
+++ lua/efmls-configs/linters/golangci_lint.lua
@@ -5,8 +5,16 @@
 local sourceText = require('efmls-configs.utils').sourceText
 local fs = require('efmls-configs.fs')
 
+local outputFormat = '--out-format tab'
+local ok, versionOutput = pcall(vim.fn.system, { 'golangci-lint', 'version' })
+
+--- check if golangci-lint is version 2
+if ok and string.match(versionOutput, 'version%s+v?(2%.[%d%.]+)') then
+  outputFormat = '--output.tab.path=stdout'
+end
+
 local linter = 'golangci-lint'
-local command = string.format('%s run --color never --out-format tab "${INPUT}"', fs.executable(linter))
+local command = string.format('%s run --color never %s "${INPUT}"', fs.executable(linter), outputFormat)
 
 return {
   prefix = linter,
