0c19e6297fac4cef76559e1be2dd2febd7bee078
Shubham Pawar <shubhampawar3007@gmail.com>
2025-06-23 13:33:04 +0530

    chore: simplify pr and switch to stylish format for eslint

--- lua/efmls-configs/linters/eslint_d.lua
+++ lua/efmls-configs/linters/eslint_d.lua
@@ -3,29 +3,11 @@
 -- url: https://github.com/mantoni/eslint_d.js
 
 local sourceText = require('efmls-configs.utils').sourceText
-local concat = require('efmls-configs.utils').concat
 local fs = require('efmls-configs.fs')
 
 local linter = 'eslint_d'
 local bin = fs.executable(linter, fs.Scope.NODE)
-
-local format = 'visualstudio'
-local lintFormats = { '%f(%l,%c): %trror %m', '%f(%l,%c): %tarning %m' }
-local appendMarkers = {}
-local ok, versionOutput = pcall(vim.fn.system, 'eslint_d --version')
-local isVersion9 = ok and string.find(versionOutput, 'eslint: v9.')
-
-if isVersion9 then
-  format = 'stylish'
-  lintFormats = { '%-P%f', '%\\s%#%l:%c %# %trror  %m', '%\\s%#%l:%c %# %tarning  %m', '%-Q,%-G%.%#' }
-  appendMarkers = {
-    'eslint.config.cjs',
-    'eslint.config.mjs',
-    'eslint.config.js',
-  }
-end
-
-local args = '--no-color --format ' .. format .. ' --stdin-filename "${INPUT}" --stdin'
+local args = '--no-color --format stylish --stdin-filename "${INPUT}" --stdin'
 local command = string.format('%s %s', bin, args)
 
 return {
@@ -33,9 +15,9 @@ return {
   lintSource = sourceText(linter),
   lintCommand = command,
   lintStdin = true,
-  lintFormats = lintFormats,
+  lintFormats = { '%-P%f', '%\\s%#%l:%c %# %trror  %m', '%\\s%#%l:%c %# %tarning  %m', '%-Q,%-G%.%#' },
   lintIgnoreExitCode = true,
-  rootMarkers = concat({
+  rootMarkers = {
     '.eslintrc',
     '.eslintrc.cjs',
     '.eslintrc.js',
@@ -43,5 +25,9 @@ return {
     '.eslintrc.yaml',
     '.eslintrc.yml',
     'package.json',
-  }, appendMarkers),
+    --- Eslint v9
+    'eslint.config.cjs',
+    'eslint.config.mjs',
+    'eslint.config.js',
+  },
 }
--- lua/efmls-configs/utils.lua
+++ lua/efmls-configs/utils.lua
@@ -7,19 +7,4 @@ M.sourceText = function(suffix)
   return string.format('efm/%s', suffix)
 end
 
----Concatenate two tables
----@param tbl1 table
----@param tbl2 table
----@return table
-M.concat = function(tbl1, tbl2)
-  local result = {}
-  for i = 1, #tbl1 do
-    result[i] = tbl1[i]
-  end
-  for i = 1, #tbl2 do
-    result[#tbl1 + i] = tbl2[i]
-  end
-  return result
-end
-
 return M
