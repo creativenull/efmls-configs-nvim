36143bef10500284c752b7663ca7f6b026a80efc
Arnold Chand <creativenull@outlook.com>
2025-11-03 12:49:16 -0400

    fix(prettier,prettierd): input path first

--- lua/efmls-configs/formatters/prettier.lua
+++ lua/efmls-configs/formatters/prettier.lua
@@ -6,7 +6,7 @@ local fs = require('efmls-configs.fs')
 
 local formatter = 'prettier'
 local command = string.format(
-  "%s --stdin --stdin-filepath ${--range-start:charStart} ${--range-end:charEnd} --config-precedence prefer-file '${INPUT}'",
+  "%s --stdin --stdin-filepath '${INPUT}' ${--range-start:charStart} ${--range-end:charEnd} --config-precedence prefer-file",
   fs.executable(formatter, fs.Scope.NODE)
 )
 
