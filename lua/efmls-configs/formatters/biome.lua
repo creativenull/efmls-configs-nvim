b175e35c5ea133e5bee42d90f91ce8419b4436a3
napisani <napisani@yahoo.com>
2025-12-02 08:41:43 -0500

    fix(formatters/biome): Update biome formatter arguments and root markers (#163)

--- lua/efmls-configs/formatters/biome.lua
+++ lua/efmls-configs/formatters/biome.lua
@@ -5,11 +5,11 @@
 local fs = require('efmls-configs.fs')
 
 local formatter = 'biome'
-local args = "check --apply --stdin-file-path '${INPUT}'"
+local args = "check --write --stdin-file-path '${INPUT}'"
 local command = string.format('%s %s', fs.executable(formatter, fs.Scope.NODE), args)
 
 return {
   formatCommand = command,
   formatStdin = true,
-  rootMarkers = { 'rome.json', 'biome.json', 'package.json' },
+  rootMarkers = { 'rome.json', 'biome.json', 'package.json', 'Biomefile' },
 }
