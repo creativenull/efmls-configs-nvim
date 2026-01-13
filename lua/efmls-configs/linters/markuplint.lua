d8e4f18d8a02c51a122a091890cab4b7cdc3aa63
Arnold Chand <creativenull@outlook.com>
2026-01-13 16:22:49 -0400

    fix(linters): fmt code

--- lua/efmls-configs/linters/markuplint.lua
+++ lua/efmls-configs/linters/markuplint.lua
@@ -10,13 +10,13 @@ local args = '--no-color --format Github ${INPUT}'
 local command = string.format('%s %s', fs.executable(linter, fs.Scope.NODE), args)
 
 return {
-	prefix = linter,
-	lintSource = sourceText(linter),
-	lintCommand = command,
-	lintStdin = true,
-	lintFormats = {
+  prefix = linter,
+  lintSource = sourceText(linter),
+  lintCommand = command,
+  lintStdin = true,
+  lintFormats = {
     '::%trror file=%.%#,line=%l,col=%c::%m',
     '::%tarning file=%.%#,line=%l,col=%c::%m',
   },
-	rootMarkers = { '.markuplintrc' },
+  rootMarkers = { '.markuplintrc' },
 }
